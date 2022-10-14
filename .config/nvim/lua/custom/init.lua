819fdaed7dc2259b5fe7693fe408b75ea94e71ca
afranco <afonso@francof.net>
2022-10-14 15:14:23 +0100

    chore: Added Latex snippets

---
+++ .config/nvim/lua/custom/init.lua
@@ -0,0 +1,3 @@
+local autocmd = vim.api.nvim_create_autocmd
+
+vim.g.luasnippets_path = "~/.config/nvim/lua/custom/luasnippets"
---
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,8 @@
+return {
+  ["L3MON4D3/LuaSnip"] = {
+    config = function()
+         require("plugins.configs.others").luasnip()
+         require("luasnip.loaders.from_vscode").lazy_load({ paths = vim.g.luasnippets_path })
+      end
+  }
+}
