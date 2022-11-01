2ca7b718328d7bbf9ae23f0a46960a774e782ff2
afranco <afonso@francof.net>
2022-11-01 00:48:17 +0000

    chore: Added baterry level

---
+++ .config/xmobar/scripts/battery.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+
+batLevel=$(acpi --battery | grep -Po '(\d+)\%' | sed 's/%//')
+
+charging=$(acpi --ac-adapter | grep -Po '(off-line|on-line)')
+
+if [ $batLevel -le 20 ];
+then
+	if [ $charging == "on-line" ];
+	then
+		echo "<fn=3> $batLevel</fn>"
+	else
+		echo "<fn=4> $batLevel</fn>"
+	fi
+elif [ $batLevel -le 30 ];
+then
+	if [ $charging == "on-line" ];
+	then
+		echo "<fn=3> $batLevel</fn>"
+	else
+		echo "<fn=4> $batLevel</fn>"
+	fi
+elif [ $batLevel -le 40 ];
+then
+	if [ $charging == "on-line" ];
+	then
+		echo "<fn=3> $batLevel</fn>"
+	else
+		echo "<fn=3> $batLevel</fn>"
+	fi
+elif [ $batLevel -le 60 ];
+then
+	if [ $charging == "on-line" ];
+	then
+		echo "<fn=3> $batLevel</fn>"
+	else
+		echo "<fn=4> $batLevel</fn>"
+	fi
+elif [ $batLevel -le 80 ];
+then
+	if [ $charging == "on-line" ];
+	then
+		echo "<fn=3> $batLevel</fn>"
+	else
+		echo "<fn=4> $batLevel</fn>"
+	fi
+elif [ $batLevel -le 95 ];
+then
+	if [ $charging == "on-line" ];
+	then
+		echo "<fn=3> $batLevel</fn>"
+	else
+		echo "<fn=4> $batLevel</fn>"
+	fi
+elif [ $batLevel -g 95 ];
+then
+	if [ $charging == "on-line" ];
+	then
+		echo "<fn=3> $batLevel</fn>"
+	else
+		echo "<fn=4> $batLevel</fn>"
+	fi
+fi
