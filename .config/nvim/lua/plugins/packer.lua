45e1be51e519af5d70e4ac01af71f0e43c2223b6
afranco <afonso@francof.net>
2022-11-07 03:25:06 +0000

    chore: Changed some vim options

--- .config/nvim/lua/plugins/packer.lua
+++ .config/nvim/lua/plugins/packer.lua
@@ -12,76 +12,83 @@ local plugins = packer.startup({function(use)
 
 
 	use{  "lervag/vimtex",
-	    	config = function ()
-	      	    require "plugins.vimtex"
-	    	end
-	  }
-  
+		config = function ()
+			require "plugins.vimtex"
+		end
+	}
+
 	use {  "ggandor/leap.nvim",
-	    	config = function ()
-	    	    require('leap').add_default_mappings()
-	    	end
-	   }
-	   
+		config = function ()
+			require('leap').add_default_mappings()
+		end
+	}
+
 	use {  "neovim/nvim-lspconfig",
-	    	config = function ()
-	    	    require "plugins.lspconfig"
-	    	end
-	  }
+		config = function ()
+			require "plugins.lspconfig"
+		end
+	}
 
 	use {  "williamboman/mason.nvim",
-	    	config = function ()
-	    	    require "plugins.mason"
-	    	end
-	  }
+		config = function ()
+			require "plugins.mason"
+		end
+	}
 
 	use {  "feline-nvim/feline.nvim" }
 
 	use {"joshdick/onedark.vim" } 
 
 	use {"nvim-tree/nvim-tree.lua",
-	 	requires = {
-		'nvim-tree/nvim-web-devicons',
+		requires = {
+			'nvim-tree/nvim-web-devicons',
 		},
 		config = function()
 			require("nvim-tree").setup()
 		end
-         }
+	}
 
 	use {"nvim-treesitter/nvim-treesitter",
 		config = function()
 			require "plugins.treesitter"
 		end
-	 }
+	}
 
-	use {'akinsho/bufferline.nvim',
-		tag = "v3.*",
+	use {'romgrk/barbar.nvim',
 		requires = 'kyazdani42/nvim-web-devicons',
 		config = function ()
-			require("bufferline").setup{}
+			require "plugins.barbar"
 		end
 	}
 
 	use {"windwp/nvim-autopairs",
-   		config = function() require("nvim-autopairs").setup {} end
+		config = function() require("nvim-autopairs").setup {} end
 	}
 
 	use {"neoclide/coc.nvim",
 		config = function() require("plugins.nvim-coc") end
 	}
 
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.0',
+		-- or                            , branch = '0.1.x',
+		requires = { {'nvim-lua/plenary.nvim'} },
+		config = function() require('plugins.telescope') end
+	}
+
+	use {"jbyuki/instant.nvim"}
 	end,
 
 	config = {
-	     auto_clean = true,
-	     compile_on_sync = true,
-	     git = { clone_timeout = 6000 },
-	     display = {
-	    	 working_sym = "ﲊ",
-	    	 error_sym = "✗ ",
-	    	 done_sym = " ",
-	    	 removed_sym = " ",
-	    	 moved_sym = "",
-	         },	
+		auto_clean = true,
+		compile_on_sync = true,
+		git = { clone_timeout = 6000 },
+		display = {
+			working_sym = "ﲊ",
+			error_sym = "✗ ",
+			done_sym = " ",
+			removed_sym = " ",
+			moved_sym = "",
+		},	
 	}
 })
