c4cb9eb777fd77688a4ad18b7e6dd599bd9ba0d6
afranco <afonso@francof.net>
2022-12-01 15:59:06 +0000

    chore: changed rust to nightly in PATH

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,12 +6,12 @@ bind \cH backward-kill-path-component
 bind "[3;5~" kill-word
 
 function config
-  /usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME $argv
+/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME $argv
 end
 
 #Vim mode
-fish_vi_key_bindings
-
+#fish_vi_key_bindings
+fish_default_key_bindings
 function vim
 	nvim $argv
 end
@@ -38,6 +38,6 @@ function u
 end
 
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/afonso/.ghcup/bin $PATH # ghcup-env
-fish_add_path /home/afonso/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/bin
+fish_add_path /home/afonso/.rustup/toolchains/nightly-x86_64-unknown-linux-gnu/bin
 fish_add_path $HOME/.local/bin 
 fish_add_path $HOME/.cargo/bin 
