acb30943c54ea45076053ee227c1d34524ec0c6d
afranco <afonso@francof.net>
2023-01-26 10:00:28 +0000

    chore: Removed empty filetype buffer from bufferline, for example, netrw

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -65,6 +65,13 @@ require("bufferline").setup({
             reveal = { 'close' }
         },
         sort_by = 'insert_after_current',
+        custom_filter = function(buf_number, buf_numbers)
+            -- filter out filetypes you don't want to see
+            print(vim.bo[buf_number].filetype)
+            if vim.bo[buf_number].filetype ~= "" then
+                return true
+            end
+        end,
     },
     highlights = require("catppuccin.groups.integrations.bufferline").get()
 })
