1b427d298df0500313795fda4b6446c4d183b461
afranco <afonso@francof.net>
2023-06-28 00:34:24 +0100

    chore: Added underline comparator to cmp

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -131,6 +131,19 @@ cmp.setup {
             cmp.config.compare.exact,
             cmp.config.compare.offset,
             cmp.config.compare.recently_used,
+
+            --Make entries that start with underline appear after
+            function(entry1, entry2)
+                local _, entry1_under = entry1.completion_item.label:find "^_+"
+                local _, entry2_under = entry2.completion_item.label:find "^_+"
+                entry1_under = entry1_under or 0
+                entry2_under = entry2_under or 0
+                if entry1_under > entry2_under then
+                    return false
+                elseif entry1_under < entry2_under then
+                    return true
+                end
+            end,
         }
     },
     sources = cmp.config.sources(
