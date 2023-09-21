113fe4617f40936be0df4316b8e4c12884e00327
afranco <afonso@francof.net>
2023-09-21 14:25:26 +0100

    chore: Added core commands for ESR

--- .config/fish/myfunctions/qol.fish
+++ .config/fish/myfunctions/qol.fish
@@ -8,3 +8,17 @@ end
 function restore --argument file
     mv $file (echo $file | sed s/.bak//)
 end
+
+function core
+    xhost +local:root
+    sudo docker exec -it core core-gui
+end
+
+function core-bash
+    sudo docker exec -it core /bin/bash 
+end
+
+function launch-term --argument nodename
+     sudo docker exec -it core xterm -bg black -fg white -fa 'DejaVu Sans Mono' -fs 16 -e vcmd -c /tmp/pycore.1/$nodename -- /bin/bash
+end
+
