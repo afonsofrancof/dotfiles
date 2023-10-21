f0899924f0ce2ef7f32a56196dd6f7e9d785c5bb
afranco <afonso@francof.net>
2023-10-21 15:18:59 +0100

    chore: Added nvim-dap for debugging c and c++ code

--- .config/nvim/lua/plugins/lazy.lua
+++ .config/nvim/lua/plugins/lazy.lua
@@ -160,7 +160,7 @@ lazy.setup({
 
     {
         'luk400/vim-jukit',
-        event = "VeryLazy",
+        ft = {'python','ipynb'},
     },
 
     {
@@ -221,6 +221,40 @@ lazy.setup({
                 }
             }
         end
+    },
+
+    {
+        "rcarriga/nvim-dap-ui",
+        event = "VeryLazy",
+        dependencies = "mfussenegger/nvim-dap",
+        config = function()
+            local dap = require("dap")
+            local dapui = require("dapui")
+            dapui.setup()
+            dap.listeners.after.event_initialized["dapui_config"] = function()
+                dapui.open()
+            end
+            dap.listeners.before.event_terminated["dapui_config"] = function()
+                dapui.close()
+            end
+            dap.listeners.before.event_exited["dapui_config"] = function()
+                dapui.close()
+            end
+        end
+    },
+    {
+        "jay-babu/mason-nvim-dap.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "mfussenegger/nvim-dap",
+        },
+        opts = {
+            handlers = {}
+        },
+    },
+    {
+        "mfussenegger/nvim-dap",
     },
 
     {
