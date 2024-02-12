e72632ed4a53268eaf1578ed0ab289df109328d7
afranco <afonso@francof.net>
2024-02-12 15:06:54 +0000

    chore(ZSH): fzf tab

--- .config/zsh/main.zsh
+++ .config/zsh/main.zsh
@@ -7,10 +7,8 @@ setopt INC_APPEND_HISTORY        # Write to the history file immediately, not wh
 setopt SHARE_HISTORY             # Share history between all sessions.
 setopt HIST_EXPIRE_DUPS_FIRST    # Expire duplicate entries first when trimming history.
 setopt HIST_IGNORE_DUPS          # Don't record an entry that was just recorded again.
-setopt HIST_IGNORE_ALL_DUPS      # Delete old recorded entry if new entry is a duplicate.
 setopt HIST_FIND_NO_DUPS         # Do not display a line previously found.
 setopt HIST_IGNORE_SPACE         # Don't record an entry starting with a space.
-setopt HIST_SAVE_NO_DUPS         # Don't write duplicate entries in the history file.
 setopt HIST_REDUCE_BLANKS        # Remove superfluous blanks before recording entry.
 setopt HIST_VERIFY
 
@@ -28,6 +26,13 @@ export PATH="$HOME/.local/bin:$PATH"
 export PATH="$HOME/.cargo/bin:$PATH"
 export PATH="$HOME/.ghcup/bin:$PATH"
 export PATH="/usr/bin/vendor_perl:$PATH"
+export PATH="/Applications/usdpython/usdpython/USD:$PATH"
+export PATH="/Applications/usdpython/usdzconvert:$PATH"
+export PATH="/Users/afonso/Library/Python/3.11/bin:$PATH"
+[ -f "/Users/afonso/.ghcup/env" ] && source "/Users/afonso/.ghcup/env" # ghcup-env
+export PYTHONPATH="/Applications/usdpython/USD/lib/python:$PYTHONPATH"
+export JAVA_HOME="/opt/homebrew/opt/openjdk@17/libexec/openjdk.jdk/Contents/Home"
+
 
 if [ "$(arch)" = "arm64" ]; then
     eval "$(/opt/homebrew/bin/brew shellenv)"
@@ -40,6 +45,7 @@ fi
 # bun
 export BUN_INSTALL="$HOME/.bun"
 export PATH="$BUN_INSTALL/bin:$PATH"
+enable-fzf-tab
 
 # opam configuration
 source $HOME/.opam/opam-init/init.zsh > /dev/null 2>&1 || true
--- .config/zsh/overrides.zsh
+++ .config/zsh/overrides.zsh
@@ -1,2 +1,3 @@
 ZSH_AUTOSUGGEST_STRATEGY=(match_prev_cmd history completion)
 ZSH_TMUX_AUTOSTART=true
+zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
