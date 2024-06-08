1868a7156c60618f12a152823a8a73131bd6b62a
afranco <afonso@francof.net>
2024-06-08 18:10:53 +0100

    chore(zsh): Added a session restore to vim when used without arguments

--- .config/zsh/replacements.zsh
+++ .config/zsh/replacements.zsh
@@ -2,7 +2,18 @@
 
 alias ls="eza --icons -l --sort type"
 
-alias vim="nvim --listen /tmp/nvim.pipe"
+function vim {
+    if [[ -z "$@" ]]; then
+        if [[ -f "./.session.vim" ]]; then
+            nvim -S .session.vim --listen /tmp/nvim.pipe -c 'lua vim.g.savesession = true'
+        else
+            nvim --listen /tmp/nvim.pipe -c 'lua vim.g.savesession = true'
+        fi
+    else
+        nvim --listen /tmp/nvim.pipe "$@"
+    fi
+}
+
 
 alias lg="lazygit"
 alias calcurse-caldav='CALCURSE_CALDAV_PASSWORD=$(keyring get caldav afonso) calcurse-caldav'
