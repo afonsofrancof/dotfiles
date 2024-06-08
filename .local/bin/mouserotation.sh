20c225b8ca91b3a31a9361cc5beebd2b5e71d23d
afranco <afonso@francof.net>
2024-06-08 18:12:01 +0100

    chore(scripts): changed rotation method on mouserotation.sh

--- .local/bin/mouserotation.sh
+++ .local/bin/mouserotation.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 for id in `xinput --list | awk '/Razer Razer DeathAdder V3 Pro .*pointer/ {print $8}' | sed 's/id=\(.*\)/\1/'`; do
     notify-send "Applied Mouse Rotation"
-    xinput set-prop $id "Coordinate Transformation Matrix" 0.9848 0.1736 0 -0.1736 0.9848 0 0 0 1
+    xinput set-prop $id "libinput Rotation Angle" 350 
     sleep 1
 done 
