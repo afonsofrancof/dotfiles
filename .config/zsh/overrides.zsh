1b6dcd987862ccc3e913d515330a0fe221579614
afranco <afonso@francof.net>
2024-06-08 18:08:35 +0100

    chore(zsh): only start TMUX if not in a TTY

--- .config/zsh/overrides.zsh
+++ .config/zsh/overrides.zsh
@@ -1,3 +1,7 @@
 ZSH_AUTOSUGGEST_STRATEGY=(match_prev_cmd history completion)
-ZSH_TMUX_AUTOSTART=true
+
+if [[ ! $(tty) =~ ^/dev/tty[0-9]*$ ]]; then
+    ZSH_TMUX_AUTOSTART=true
+fi
+
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
