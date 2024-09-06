24b93ba30efdf35af4b8352f0fdf600f73168e07
afonso <afonso@francof.net>
2024-09-07 00:54:06 +0100

    [wezterm] added wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,27 @@
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+--Font config
+config.font_size = 16
+
+--Color scheme
+config.color_scheme = 'Gruvbox dark, hard (base16)'
+
+-- Remove tab bar
+config.enable_tab_bar = false
+
+-- Make it borderless
+config.window_decorations = 'RESIZE'
+
+-- Remove anoying things
+config.window_close_confirmation = 'NeverPrompt'
+config.audible_bell = "Disabled"
+
+config.set_environment_variables = {
+    TERM = 'xterm-256color'
+}
+
+config.bold_brightens_ansi_colors = "No"
+
+return config
