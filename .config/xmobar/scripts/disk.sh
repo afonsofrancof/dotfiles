45ea35660bbb3324db7980ac48772e49cf327c99
afonso <afonso@francof.net>
2024-09-07 03:05:26 +0100

    [xmobar] Added gruvbox theme

--- .config/xmobar/scripts/disk.sh
+++ .config/xmobar/scripts/disk.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 freespace=$(btrfs fi usage / | grep -Po 'Free \(estimated\):.*' | awk '{print $3}')
-echo "<fn=6></fn>  <fc=#AAC0F0> $freespace</fc>"
+echo "<fc=#7C6F64><fn=6></fn></fc>  <fc=#B8BB26> $freespace</fc>"
