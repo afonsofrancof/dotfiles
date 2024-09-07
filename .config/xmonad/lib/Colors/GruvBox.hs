35b55652650c2c7ed17f2c860092727baa7a29ed
afranco <afonso@francof.net>
2024-09-07 03:05:48 +0100

    chore(xmonad): Addded gruvbox theme and changed to wezterm

---
+++ .config/xmonad/lib/Colors/GruvBox.hs
@@ -0,0 +1,17 @@
+module Colors.GruvBox where
+
+import XMonad
+
+colorScheme = "gruvbox"
+
+colorTrayer :: String
+colorTrayer = "--tint 0x282828"
+
+blue = "#83A598"
+aqua = "#8EC07C"
+orange = "#FE8019"
+red = "#FB4934"
+green = "#B8BB26"
+gray0 = "#7C6F64"
+gray1 = "#928374"
+gray2 = "#A89984"
