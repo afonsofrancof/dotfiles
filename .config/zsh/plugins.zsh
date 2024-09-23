086b482cead049c47103874c46fa34b74716b6f7
afonso <afonso@francof.net>
2024-09-23 05:04:05 +0100

    [zsh] fix completions and fzf-tab

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -1,4 +1,8 @@
+#Completions
 ZSH_AUTOSUGGEST_STRATEGY=(match_prev_cmd history completion)
+zstyle ':completion:*' rehash true
+fpath+=$ZDOTDIR/plugins/zsh-completions/src
+autoload -U compinit && compinit
 source $ZDOTDIR/plugins/zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
 
 source $ZDOTDIR/plugins/zsh-history-substring-search/zsh-history-substring-search.plugin.zsh
