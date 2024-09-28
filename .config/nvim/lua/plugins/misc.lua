75aa5bd64fc5e972718d618ee8824ac5012d28f0
afonso <afonso@francof.net>
2024-09-28 16:40:15 +0100

    [nvim] add cellular-automaton for a fml experience

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -26,4 +26,10 @@ return {
         cmd = { "Pastify" },
         opts = {},
     },
+    {
+        'eandrju/cellular-automaton.nvim',
+        config = function()
+            vim.keymap.set("n", "<leader>fml", "<cmd>CellularAutomaton make_it_rain<CR>")
+        end
+    }
 }
