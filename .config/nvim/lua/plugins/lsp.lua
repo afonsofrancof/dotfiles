cf5d16e7927c9ddde78d823052f1005cf2c53be2
afonso <afonso@francof.net>
2024-10-11 15:13:34 +0100

    [nvim] dap and dap ui

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -232,6 +232,39 @@ return {
         config = true,
     },
     {
+        "rcarriga/nvim-dap-ui",
+        event = "VeryLazy",
+        dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+        config = function()
+            local dap = require("dap")
+            local dapui = require("dapui")
+            dapui.setup()
+            dap.listeners.after.event_initialized["dapui_config"] = function()
+                dapui.open()
+            end
+            dap.listeners.before.event_terminated["dapui_config"] = function()
+                dapui.close()
+            end
+            dap.listeners.before.event_exited["dapui_config"] = function()
+                dapui.close()
+            end
+        end
+    },
+    {
+        "jay-babu/mason-nvim-dap.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "mfussenegger/nvim-dap",
+        },
+        opts = {
+            handlers = {}
+        },
+    },
+    {
+        "mfussenegger/nvim-dap",
+    },
+    {
         "stevearc/conform.nvim",
         event = "VeryLazy",
         config = function()
@@ -251,7 +284,7 @@ return {
 
     {
         "lervag/vimtex",
-        filetypes = {"tex"},
+        filetypes = { "tex" },
         config = function()
             if vim.loop.os_uname().sysname == "Darwin" then
                 vim.g.vimtex_view_method = 'skim'
