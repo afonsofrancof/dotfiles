5144c74839629dd6e40fbc996c9620771356fcc7
afonso <afonso@francof.net>
2024-10-11 15:13:51 +0100

    [nvim] mini.ai and treesitter-textobjects

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -38,6 +38,18 @@ return {
         'echasnovski/mini.ai',
         event = "VeryLazy",
         version = false,
-        opts = {}
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter-textobjects'
+        },
+        config = function()
+            local gen_spec = require('mini.ai').gen_spec
+            require('mini.ai').setup({
+                custom_textobjects = {
+                    -- Function definition (needs treesitter queries with these captures)
+                    F = gen_spec.treesitter({ a = '@function.outer', i = '@function.inner' }),
+                },
+                n_lines = 1000
+            })
+        end
     },
 }
