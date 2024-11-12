d077ffe4380b5dbae309806260a8cc4253fc3d22
afranco <afonso@francof.net>
2024-11-12 15:41:16 +0000

    chore(nvim): hardtime.nvim

--- .config/nvim/lua/plugins/editing.lua
+++ .config/nvim/lua/plugins/editing.lua
@@ -51,5 +51,10 @@ return {
             },
 
         },
-    }
+    },
+    {
+        "m4xshen/hardtime.nvim",
+        dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+        opts = {}
+    },
 }
