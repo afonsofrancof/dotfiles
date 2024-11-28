c4ab75c313e5df55123eb099f0b7aa0fc3a58151
afonso <afonso@francof.net>
2024-11-28 18:32:53 +0000

    [nvim] some stuff including clipboard stuff

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -9,3 +9,13 @@ vim.keymap.set("n", "<A-h>", "<cmd>bp<CR>")
 vim.keymap.set("n", "<A-l>", "<cmd>bn<CR>")
 --jk as escape
 vim.keymap.set("i", "jk","<esc>")
+-- Copy to system clipboard
+vim.keymap.set({ 'n', 'x' }, 'gy', '"+y', { desc = 'Copy to system clipboard' })
+-- Paste from system clipboard in normal mode
+vim.keymap.set('n', 'gp', '"+p', { desc = 'Paste from system clipboard' })
+-- Paste from system clipboard in visual mode without overwriting the clipboard
+vim.keymap.set('x', 'gp', '"+P', { desc = 'Paste from system clipboard' })
+--Center screen after some motions
+vim.keymap.set("n", "<C-d>", "<C-d>zz", {desc = "Center cursor after moving down half-page"})
+vim.keymap.set("n", "<C-u>", "<C-u>zz", {desc = "Center cursor after moving down half-page"})
+vim.keymap.set("n", "G", "Gzz", {desc = "Center cursor after moving down half-page"})
