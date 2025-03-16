5820f5e1ab28a536a7de1c707395611a8991a868
afranco <afonso@francof.net>
2025-03-16 01:40:43 +0000

    chore(aerospace): add aerospace config

--- .config/nvim/lua/core/theme.lua
+++ .config/nvim/lua/core/theme.lua
@@ -1,4 +1,5 @@
-vim.g.everforest_better_performance=1
-vim.g.everforest_background="hard"
+vim.g.gruvbox_material_better_performance=1
+vim.g.gruvbox_material_background="hard"
+vim.g.gruvbox_material_foreground="original"
 vim.opt.background="dark"
-vim.cmd('colorscheme everforest')
+vim.cmd('colorscheme gruvbox-material')
