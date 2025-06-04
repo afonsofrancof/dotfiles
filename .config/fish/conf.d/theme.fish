384a368aea2e5df25036890ea0fcbb68ded9ef13
afranco <afonso@francof.net>
2025-06-04 14:54:51 +0100

    feat(fish): add theme

---
+++ .config/fish/conf.d/theme.fish
@@ -0,0 +1,25 @@
+set -g fish_color_normal normal
+
+set -g fish_color_command blue
+set -g fish_color_quote yellow
+set -g fish_color_redirection cyan --bold
+set -g fish_color_end green
+set -g fish_color_error brred
+set -g fish_color_param cyan
+
+set -g fish_color_autosuggestion brblack
+set -g fish_color_comment red
+
+set -g fish_color_cancel --background=red
+set -g fish_color_escape brcyan
+set -g fish_color_history_current normal --bold
+set -g fish_color_operator brcyan
+set -g fish_color_search_match white --background=brblack
+set -g fish_color_selection white --bold --background=brblack
+set -g fish_color_status red
+set -g fish_color_valid_path normal --underline
+
+set -g fish_pager_color_description yellow --italic
+set -g fish_pager_color_prefix normal --bold --underline
+set -g fish_pager_color_progress brwhite --background=cyan
+set -g fish_pager_color_selected_background --background=red
